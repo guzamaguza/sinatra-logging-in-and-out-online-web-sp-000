c95df1d4ba25c19589cfcaa33ea58bacc9aefffb
Mark A Guza <guzamaguza@gmail.com>
2020-05-21 03:04:12 +0000

    Automatically backed up by Learn

--- spec/sinatra_logging_in_and_out_spec.rb
+++ spec/sinatra_logging_in_and_out_spec.rb
@@ -17,7 +17,7 @@ describe 'ApplicationController' do
     before do
       @user1 = User.create(:username => "skittles123", :password => "iluvskittles", :balance => 1000)
       @user2 = User.create(:username => "flatiron4lyfe", :password => "Rubie!", :balance => 500)
-      @user3 = User.create(:username => "kittens1265", :password => "crazycatlady", :balance => 1000)
+      @user3 = User.create(:username => "kittens1265", :password => "crazycatlady", :balance => 10000)
     end
 
     it "returns a 302 redirect status code" do
